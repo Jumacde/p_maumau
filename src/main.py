15ccafccb4051498f23fd00b11d23c5fea6fafb5
Yuma Takamura <s1729686@stud.uni-frankfurt.de>
2026-02-17 20:17:15 +0100

    commited

--- src/main.py
+++ src/main.py
@@ -1,11 +1,32 @@
 class Maumau():
     def __init__(self):
+        self.setNumOfPlayers()
+        self.inputName()
 
+def setNumOfPlayers(self):
+    self.numOfPlayers = int(input("how many players? 2-4: "))
+    numOfPlayers = self.numOfPlayers
+
+    if 2 < numOfPlayers <= 4:
+        return numOfPlayers
+    else:
+        print("invalid number of players")
+    return setNumOfPlayers(self)
+   
+
+def inputName(self):
+    numOfPlayers = self.numOfPlayers
+    name = self.playerNames
+    name = []
+
+    for i in range(numOfPlayers):
+        name.append(input("player" + str(i + 1) + " is: "))
+        return name
 
 def main():
+    setNumOfPlayers()
+    inputName()
     
-
-
 
 if __name__ == "main":    
     Maumau()
