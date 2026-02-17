861e7b9c92574e0ac441e9c8db38d08a49707ff8
Yuma Takamura <s1729686@stud.uni-frankfurt.de>
2026-02-17 20:38:23 +0100

    Commited

--- src/main.py
+++ src/main.py
@@ -5,13 +5,12 @@ class Maumau():
 # input number of players
 def setNumOfPlayers():
     numOfPlayers = int(input("input number of players(2-4):"))
-    if numOfPlayers <= 4:
-        print("player number is " + str())
+    if 2 <=numOfPlayers <= 4:
+        print("player number is " + str(numOfPlayers))
         return numOfPlayers
     else:
         print("error: number of players must be between 2 and 4")   
         return setNumOfPlayers()
-
 
 def main():
     setNumOfPlayers()
